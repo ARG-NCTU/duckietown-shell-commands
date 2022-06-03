742ecd433279120f76b18c4035e726df10369fb7
hchengwang <hchengwang@gmail.com>
2022-06-03 16:52:35 +0800

    hello duckiepond

---
+++ duckiepond/command.py
@@ -0,0 +1,8 @@
+from dt_shell import DTCommandAbs, DTShell
+
+
+class DTCommand(DTCommandAbs):
+    @staticmethod
+    def command(shell: DTShell, args):
+        print("Hello Duckiepond!")
+        exit()
